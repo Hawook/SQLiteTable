677ba62670c29a07a4ec5f87156a9c970d5bf87c
Hawook <Hawook@users.noreply.github.com>
2020-03-23 00:59:01 +0500

    UPD

--- Unit1.cpp
+++ Unit1.cpp
@@ -4,6 +4,10 @@
 #pragma hdrstop
 
 #include "Unit1.h"
+#include <string>
+#include "sqlite3.h"
+
+#pragma link "VirtualTrees"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
@@ -12,5 +16,32 @@ TForm1 *Form1;
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
 {
+	VSTree->NodeDataSize = sizeof(VSTStruct); // размер по структуре
 }
 //---------------------------------------------------------------------------
+void __fastcall TForm1::OpenButtonClick(TObject *Sender)
+{
+	VSTree -> Clear(); // очистка дерева
+}
+void __fastcall TForm1::ClearAllButtonClick(TObject *Sender)
+{
+	VSTree -> Clear(); // очистка дерева
+}
+void __fastcall TForm1::RemoveButtonClick(TObject *Sender)
+{
+	VSTree -> Clear(); // очистка дерева
+}
+void __fastcall TForm1::VSTreeGetText(TBaseVirtualTree *Sender, PVirtualNode Node,
+          TColumnIndex Column, TVSTTextType TextType, UnicodeString &CellText)
+
+{
+	if(!Node) return;
+	VSTStruct *nodeData = (VSTStruct*)Sender->GetNodeData(Node);
+	switch(Column)
+	{
+		case 0: CellText = nodeData->id; break;
+		case 1: CellText = nodeData->url; break;
+		case 2: CellText = nodeData->title; break;
+		case 3: CellText = nodeData->visit_count; break;
+	}
+}
