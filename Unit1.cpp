cacd548b3e164e4b93f5d41c5102f13746e26b22
Hawook <Hawook@users.noreply.github.com>
2020-03-24 15:51:20 +0500

    Кнопка открытия БД
    Добавил кнопку открытия файла БД с маской history.sqlite.
    Используя, возвращаемое значение SQLite3Stmt метода sqlite3_prepare_v2, прописал проверку для исключения ошибок при SQL-запросах в БД.

--- Unit1.cpp
+++ Unit1.cpp
@@ -24,10 +24,33 @@ void __fastcall TForm1::OpenButtonClick(TObject *Sender)
 	VSTree -> Clear(); // очистка дерева
 	sqlite3 *DB = NULL;
 	if (sqlite3_open("history.sqlite", &DB)) {
-		LabelStatus->Caption = "Error";
+		LabelStatus->Caption = "Ошибка чтения БД";
 	}
 	sqlite3_stmt *pullStatement; // возвращаемое значение
 	const char *sqlSelect = "select * from urls"; // SQL-запрос вывода таблицы
+	int result = sqlite3_prepare_v2(DB, sqlSelect, -1, &pullStatement, NULL); // переменная для проверки на ошибки возвращаемого значения  SQLite3Stmt
+    if (result != SQLITE_OK) // если запрос закончился ошибкой
+	{
+		const char* errmsg = sqlite3_errmsg(DB); // получение текста ошибки SQL
+		LabelStatus->Caption = errmsg; // вывод ошибки запроса
+	}
+	else
+	{
+		wchar_t* stringToShow;
+		VSTree->BeginUpdate(); // запрет изменений таблицы
+		while (sqlite3_step(pullStatement) != SQLITE_DONE)
+		{
+			PVirtualNode entryNode = VSTree->AddChild(VSTree->RootNode);
+			VSTStruct *nodeData = (VSTStruct*)VSTree->GetNodeData(entryNode);
+			nodeData->id = sqlite3_column_int(pullStatement, 0);
+			nodeData->url  = (wchar_t*)sqlite3_column_text16(pullStatement, 1);
+			nodeData->history.sqlitetitle  = (wchar_t*)sqlite3_column_text16(pullStatement, 2);
+			nodeData->visit_count = sqlite3_column_int(pullStatement, 3);
+		}
+		VSTree->EndUpdate(); // отрисовка таблицы
+	}
+	sqlite3_finalize(pullStatement); // очистка вывода
+	sqlite3_close(DB); // освобождение БД
 }
 void __fastcall TForm1::ClearAllButtonClick(TObject *Sender)
 {
